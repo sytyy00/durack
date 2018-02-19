a595e50bfd8c5bf3d4fd05307a617dcd79c1ca16
DESKTOP-ERQS2NV\sytyy <sytyy00@gmail.com>
2018-02-20 00:11:18 +0300

    update logs,add method
    method number_card in class of Game
    logs in file and console

--- log.py
+++ log.py
@@ -1,17 +1,21 @@
 import logging
-
+import sys
 game_logger = logging.getLogger("Game")
 game_logger.setLevel(logging.INFO)
-handler_1 = logging.FileHandler('log_1.log', 'a')
+handler_1 = logging.FileHandler('log_game.log', 'a')
 handler_1.setLevel(logging.INFO)
-formatter_1 = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler_1.setFormatter(formatter_1)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler_1.setFormatter(formatter)
 game_logger.addHandler(handler_1)
 
 error = logging.getLogger("Error")
-error.setLevel(logging.INFO)
-handler_2 = logging.FileHandler('log_2.log', 'a')
-handler_2.setLevel(logging.INFO)
-formatter_2 = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler_2.setFormatter(formatter_2)
-error.addHandler(handler_2)
+error.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('log_errors.log', 'a')
+file_handler.setLevel(logging.DEBUG)
+#formatter_2 = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+error.addHandler(file_handler)
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+error.addHandler(console_handler)
